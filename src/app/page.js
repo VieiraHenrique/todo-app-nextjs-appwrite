33c124634c7e1e3e5bffab127342975859f4ed5b
Henrique Andrade Vieira <me.henriquevieira@gmail.com>
2023-11-30 12:33:16 +0100

    save

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 "use client";
 
+import { account } from "@/appwrite.js/config";
 import React, { useEffect } from "react";
 
 export default function HomePage() {
   useEffect(() => {
-    console.log("test");
+    console.log(account);
   }, []);
 
   return <div>HomePage</div>;
